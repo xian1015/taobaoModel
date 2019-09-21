5d63c413a14682f7b31524b94c2b94545e55e8f0
FancyYumi <963368116@qq.com>
2019-09-21 11:59:17 +0800

    更新

--- client/src/components/lzh.vue
+++ client/src/components/lzh.vue
@@ -1,91 +1,81 @@
 <template>
-<div class="bigbox" v-title data-title="Zara官方旗舰店-淘宝搜索">
-  <div class="smallbox" v-for="(item,index) in object " :key="index" @click="fun">
-   
-    <router-link :to="{path:'/goods'}">
-      <img class="imge" :src="item.url">
-      <div class="price">
-      ¥{{item.price}}
-      </div>
-      <div class="text">{{item.text}}</div>
-    </router-link>
+  <div class="bigbox" v-title data-title="Zara官方旗舰店-淘宝搜索">
+    <div class="smallbox" v-for="(item,index) in object " :key="index" @click="fun">
+      <router-link :to="{path:'/goods'}">
+        <img class="image" :src="item.url" />
+        <div class="price">¥{{item.price}}</div>
+        <div class="text">{{item.text}}</div>
+        <div class="shops">Zara官方旗舰店</div>
+      </router-link>
+    </div>
+
+    <router-view></router-view>
   </div>
-  
- 
- 
-  <router-view> </router-view>
-</div>
 </template>
 <script>
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios)
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 export default {
-  
-  data:function(){
-      return {object:[]}
+  data: function() {
+    return { object: [] };
   },
-  name: 'xinlang',
-  mounted(){
-    Vue.axios.get(`http://localhost:8888/index`).then((response) => {
-        this.object=response.data
-        console.log(response.data)
-    
-})
+  name: "xinlang",
+  mounted() {
+    Vue.axios.get(`http://localhost:8888/index`).then(response => {
+      this.object = response.data;
+    });
   },
-  props: {
-    
-  },
-  
-  methods:{
-    fun:function(){
-      this.$router.push()
+  methods: {
+    fun: function() {
+      this.$router.push();
     }
-    
   }
-}
-
-
-
-
-
+};
 </script>
 <style scoped>
-.bigbox{
-  width: 800px;
+.bigbox {
+  width: 1000px;
   height: 700px;
   margin: 40px auto;
   display: flex;
   justify-content: space-around;
-  align-content: space-around;
-  flex-wrap: wrap
+  flex-wrap: wrap;
 }
-.smallbox{
-  width: 200px;
+.smallbox {
+  margin-right: 20px;
+  width: 220px;
   height: 324px;
- 
+  border: 0.1px solid lightgrey;
+  padding: 1px;
 }
-.imge{
+.image {
+  margin-bottom: 6px;
   width: 100%;
   height: 220px;
 }
-.price{
+.price {
   margin: 0 auto;
-  width: 170px;
-  font-size: 22px;
-  color:red;
+  width: 180px;
+  font-size: 18px;
+  font-weight: 600;
+  color: red;
 }
-a{
+a {
   text-decoration: none;
-
 }
-.text{
-  width: 170px;
+.text {
+  width: 190px;
   margin: 0 auto;
   color: black;
-  font-size: 13px;
-  
+  font-size: 12px;
 }
-
+.shops {
+  width: 190px;
+  margin: 0 auto;
+  color: gray;
+  font-size: 12px;
+  padding-top: 6px;
+}
 </style>
